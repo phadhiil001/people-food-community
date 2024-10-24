243125964d020d3fda7de71f259ab2d33c1a4d1e
phadhiil001 <phadhiil001@gmail.com>
2024-10-24 09:54:17 -0500

    Added a couple of components and paged fro routing

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import classes from './page.module.css'
+import Link from 'next/link'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-const MealsPage = () => {
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
   return (
-    <h1>Meals</h1>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favourite recipe and cook it yourself. It is easy and fun</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your favorite recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals....</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   )
 }
 
-export default MealsPage
