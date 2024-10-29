a42f119b57ac1dc26a77603026b37c96662a64ca
phadhiil001 <phadhiil001@gmail.com>
2024-10-29 10:59:15 -0500

    Final Commit

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,12 +4,26 @@ import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
 import { notFound } from 'next/navigation'
 
+export async function generateMetadata({params}){
+  const meal = await getMeal(params.mealSlug);
+
+  if(!meal) {
+    notFound();
+  }
+
+  return{
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 const MealDetailsPage = ({params}) => {
   const meal = getMeal(params.mealSlug);
 
   if(!meal) {
     notFound();
   }
+
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
